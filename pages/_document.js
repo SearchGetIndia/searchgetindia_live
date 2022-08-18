eb0d3117788b4dfb69912b653e7885da37ce26b6
spider-sagar <sagar.shirsat@spiderweb-tech.com>
2022-08-18 22:17:12 +0530

    custome css

---
+++ pages/_document.js
@@ -0,0 +1,50 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
+export default function Document({title,keywords, Description, children}) {
+  return (
+    <Html>
+
+    <Head>
+    
+    <meta charset="utf-8" />
+    {/* <meta http-equiv="X-UA-Compatible" content="IE=edge" /> */}
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0" />
+    <meta name="description" content={Description}/>
+    <meta name="keywords" content={keywords}/>
+
+    <title>{title}</title>
+
+    
+    <link rel="shortcut icon" href="assets/images/favicon.ico" type="image/x-icon" defer/>
+  
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css" />
+    
+  
+    <link rel="stylesheet" type="text/css" href="assets/font-awesome/css/font-awesome.min.css" />
+  
+ 
+
+      </Head>
+
+      <body>
+        <Main />
+        <NextScript />
+        <a href="#" className="scroll-to-top"><i className="fa fa-angle-up"></i></a>
+    <script type="text/javascript" src="assets/js/jquery.min.js" defer ></script>
+    <script type="text/javascript" src="assets/js/bootstrap.min.js" defer ></script>
+    
+    {/* <!-- custom JS --> */}
+    <script type="text/javascript" src="assets/js/custom.js" defer></script>
+   
+
+      </body>
+      
+    </Html>
+  )
+}
+
+ Document.defaultProps = {
+   title : "SearchGetIndia",
+   Description : "NA",
+   keywords : "SearchGetIndia"
+ }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,110 @@
+// export const API_URL = process.env.NEXTJS_PUBLIC_URL || "http://localhost:1337";
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import FooterBar from '../Layout/FooterBar';
+import NavBar from '../Layout/NavBar';
+import React, { useState, useEffect  } from 'react';
+import Router, { useRouter } from 'next/router';
+import { BASE_URL } from '../config/configurl';
 
-export default function Home() {
+
+
+export async function getServerSideProps() 
+{
+  const res = await fetch(`${BASE_URL}/api/servicelists?populate=*`);
+  // const res = await fetch('https://searchgetadmin.herokuapp.com/api/servicelists?populate=*');
+  // const res = await fetch('http://localhost:1337/api/servicelists');
+
+  const service_list = await res.json()
+  return { 
+    props: { service:service_list },
+  }
+}
+
+
+
+function Home (props) 
+{
+  
+
+
+  const router = useRouter();
+  let sendPropts = (servive_name)=>
+
+  {
+   
+    const service = servive_name;
+    Router.push({ pathname: "/demo", query:{ service }})
+   }
+
+
   return (
-  <>
-  <h3>Testing Project Updated</h3>
-  </>
+    <>
+
+      {/* navbar include here */}
+      <NavBar />
+
+
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12">
+                   
+
+              {
+                   props.service.data.map((item , index)=>
+                   {
+                   return(
+        <div key={index}>
+
+            <div className="col-md-6 brd">
+              <div className="m-4">
+                <div className="card11">
+                  <div className="row">
+                    <div className="col-sm-5">
+                      {/* movies.movie_poster.url */}
+                       <img className="card-img-top h-100" alt="..."  src={`${BASE_URL}`+ item.attributes.image.data.attributes.url} />
+
+                      {/* <Image src={item.attributes.image.data.attributes.name} width={500} height={500}  className="card-img-top h-100"/> */}
+                    
+                    
+                    </div>
+                    <div className="col-sm-7">
+                      <div className="BusinessNamelist">{item.attributes.Service_Name}</div>
+                      <div className="card-body">
+                        <p>{item.attributes.Service_Desc}</p>
+                        {/* <p>{console.log(item.attributes.image.data.attributes.name)}</p> */}
+                         {/* <a onClick={()=> sendPropts()} className="btn btn-primary">Find Location <i className="fa fa-map-marker"></i></a> */}
+
+                         <a onClick={(e)=>{sendPropts(item.attributes.Service_Name)}}
+ 
+                          className="btn btn-primary">Find Location <i className="fa fa-map-marker"></i></a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+               </div>
+            </div>
+            
+        </div>
+
+           )
+          }
+        )
+        }
+
+
+
+          
+
+              
+
+          </div>
+        </div>
+      </div>
+
+      {/* footer include here */}
+      <FooterBar />
+    </>
   )
 }
+export default Home;
