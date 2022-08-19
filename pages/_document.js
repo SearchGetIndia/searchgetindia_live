76d9e49b55781f2baa638c9b4babea5ebc1c6517
spider-sagar <sagar.shirsat@spiderweb-tech.com>
2022-08-19 19:10:52 +0530

    link

--- pages/_document.js
+++ pages/_document.js
@@ -4,39 +4,14 @@ export default function Document({title,keywords, Description, children}) {
   return (
     <Html>
 
-    <Head>
-    
-    <meta charset="utf-8" />
-    {/* <meta http-equiv="X-UA-Compatible" content="IE=edge" /> */}
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0" />
-    <meta name="description" content={Description}/>
-    <meta name="keywords" content={keywords}/>
-
-  
-
-{/*     
-    <link rel="shortcut icon" href="assets/images/favicon.ico" type="image/x-icon" />
-  
-    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css" />
-    
-  
-    <link rel="stylesheet" type="text/css" href="assets/font-awesome/css/font-awesome.min.css" />
-   */}
- 
-
-      </Head>
+   
 
       <body>
         <Main />
         <NextScript />
         <a href="#" className="scroll-to-top"><i className="fa fa-angle-up"></i></a>
-    {/* <script type="text/javascript" src="assets/js/jquery.min.js" defer ></script>
-    <script type="text/javascript" src="assets/js/bootstrap.min.js" defer ></script>
-    <script type="text/javascript" src="assets/js/custom.js" defer></script> */}
-    
-    {/* <!-- custom JS --> */}
-    
    
+     
 
       </body>
       
