2f1ae22d3223cab761e82d42952a034b070edf16
spider-sagar <sagar.shirsat@spiderweb-tech.com>
2022-08-20 21:00:04 +0530

    search page result done 1

--- next.config.js
+++ next.config.js
@@ -1,22 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  trailingSlash: true,
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    concurrentFeatures: true,
-    serverComponents: true,
-  },
 }
 
 module.exports = nextConfig
-
-
-
-
-
-
-
-
-
-
