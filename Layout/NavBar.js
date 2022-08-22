c9e9e60b5ab36279b4598a7223da681ca676f284
spider-sagar <sagar.shirsat@spiderweb-tech.com>
2022-08-22 20:15:29 +0530

    new

--- Layout/NavBar.js
+++ Layout/NavBar.js
@@ -14,7 +14,21 @@ const NavBar = () => {
  
   return (
   <>
- 
+ <Head>
+        
+
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css"/>
+       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" integrity="sha512-5A8nwdMOWrSz20fDsjczgUidUBR8liPYU+WymTZP1lmY9G6Oc7HlZv156XqnsgNUzTyMefFTcsFH/tnJE/+xBg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+       
+           <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.1/jquery.min.js" integrity="sha512-nhY06wKras39lb9lRO76J4397CH1XpRSLfLJSftTeo3+q2vP7PaebILH9TqH+GRpnOhfAGjuYMVmVTOZJ+682w==" crossorigin="anonymous" referrerpolicy="no-referrer" defer></script>
+       
+           <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js" defer></script>
+       
+           {/* <script type="text/javascript" src="assets/js/custom.js" defer></script> */}
+           <script type="text/javascript" src="https://searchgetindia.vercel.app/assets/js/custom.js" defer></script>
+       
+             </Head>
 
 <div className="navigationBar">
         <div className="container topHeadcontainer">
