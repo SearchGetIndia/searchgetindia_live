99a90fb330e9377114b6dbb3a5e26dcb635bb6b8
spider-sagar <sagar.shirsat@spiderweb-tech.com>
2022-08-22 20:21:06 +0530

    new1

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,17 @@ export default function Document({title,keywords, Description, children}) {
   return (
     <Html>
 
+    <Head>
+    
+    <meta charset="utf-8" />
+  
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0" />
+    <meta name="description" content={Description}/>
+    <meta name="keywords" content={keywords}/>
 
+    
+
+      </Head>
 
       <body>
         <Main />
@@ -15,4 +25,3 @@ export default function Document({title,keywords, Description, children}) {
     </Html>
   )
 }
-
